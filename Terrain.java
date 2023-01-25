12ac75dd51aa1720f91ece4924348721df6d3d61
sbedev <sbedev88@gmail.com>
2023-01-25 14:17:30 +0100

    classe terrain

---
+++ Terrain.java
@@ -0,0 +1,24 @@
+package org.sbe.toto;
+
+import com.sun.tools.classfile.ConstantPool;
+
+public class Terrain {
+    Long superficie;
+
+    public Terrain() {
+    }
+
+    public Terrain(Long superficie) {
+        this.superficie = superficie;
+    }
+
+    public Long getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(Long superficie) {
+        this.superficie = superficie;
+    }
+
+
+}
