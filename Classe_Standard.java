3aab9cf24734dfd57c5c93a0e2065ea28ea0ce76
aurelien <aurelienvenel@gmail.com>
2023-01-25 14:18:22 +0100

    ajout classe Classe_Standard

---
+++ Classe_Standard.java
@@ -0,0 +1,36 @@
+public class Classe_Standard()
+{
+    private int Code_Classe;
+    private char Type_Bien;
+    private int Prix_Maximum;
+
+    public int getCode_Classe()
+    {
+        return Code_Classe;
+    }
+
+    public char getType_Bien()
+    {
+        return Type_Bien;
+    }
+
+    public int getPrix_Maximum()
+    {
+        return Prix_Maximum;
+    }
+
+    public int setCode_Classe()
+    {
+        this.Code_Classe = Code_Classe;
+    }
+
+    public char setType_Bien()
+    {
+        this.Type_Bien = Type_Bien;
+    }
+    
+    public int setPrix_Maximum()
+    {
+        this.Prix_Maximum = Prix_Maximum;
+    }
+}
