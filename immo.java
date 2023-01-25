31454b93967e1ea92c239bf51876633c3ba7ac1f
Sylvain_C <scarlier@cutii.io>
2023-01-25 14:37:04 +0100

    first commit branche immo

---
+++ immo.java
@@ -0,0 +1,45 @@
+public class immo {
+    private String status;
+    private String localisation;
+    private Boolean etatdubien;
+    private Integer prixachat;
+    private Integer loyermensuel;
+    private Integer chargemensuel;
+
+    public immo(String status, String localisation, Boolean etatdubien, Integer prixachat, Integer loyermensuel,
+            Integer chargemensuel) {
+
+        this.status = status;
+        this.localisation = localisation;
+        this.etatdubien = etatdubien;
+        this.prixachat = prixachat;
+        this.loyermensuel = loyermensuel;
+        this.chargemensuel = chargemensuel;
+
+    }
+
+    public String getStatut() {
+        return status;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public Boolean getEtatdubien() {
+        return etatdubien;
+    }
+
+    public Integer getPrixachat() {
+        return prixachat;
+    }
+
+    public Integer getLoyermensuel() {
+        return loyermensuel;
+    }
+
+    public Integer getChargemensuel() {
+        return chargemensuel;
+    }
+
+}
