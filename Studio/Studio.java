7ac6036b76a4498a960eda0d8209ba6fcfb51cf9
Quentin C <qchevriaux@hotmail.fr>
2023-01-25 14:33:23 +0100

    Ajout de studio

---
+++ Studio/Studio.java
@@ -0,0 +1,34 @@
+public class Studio{
+    String Description;
+    String Loc_etage;
+
+    public String getDescription() {
+        return this.Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public String getLoc_etage() {
+        return this.Loc_etage;
+    }
+
+    public void setLoc_etage(String Loc_etage) {
+        this.Loc_etage = Loc_etage;
+    }
+
+    public Boolean isAscensseur() {
+        return this.Ascensseur;
+    }
+
+    public Boolean getAscensseur() {
+        return this.Ascensseur;
+    }
+
+    public void setAscensseur(Boolean Ascensseur) {
+        this.Ascensseur = Ascensseur;
+    }
+    Boolean Ascensseur;
+
+}
