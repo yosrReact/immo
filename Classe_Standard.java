ce421c5ff8edaec3f78a5697b46e0cc881dbbf01
aurelien <aurelienvenel@gmail.com>
2023-01-25 14:26:54 +0100

    ajout, modif classe Classe_Standard

--- Classe_Standard.java
+++ Classe_Standard.java
@@ -1,43 +1,43 @@
 public class Classe_Standard()
 {
-    private int Code_Classe;
-    private String Type_Bien;
-    private int Prix_Maximum;
+    private int code_Classe;
+    private String type_Bien;
+    private int prix_Maximum;
 
     public Classe_Standard()
     {
-        this.Type_Bien = Type_bien;
-        this.Code_Classe = Code_Classe;
-        this.Prix_Maximum = Prix_Maximum;
+        this.type_Bien = type_bien;
+        this.code_Classe = code_Classe;
+        this.prix_Maximum = prix_Maximum;
     }
 
     public int getCode_Classe()
     {
-        return Code_Classe;
+        return code_Classe;
     }
 
     public char getType_Bien()
     {
-        return Type_Bien;
+        return type_Bien;
     }
 
     public int getPrix_Maximum()
     {
-        return Prix_Maximum;
+        return prix_Maximum;
     }
 
     public int setCode_Classe()
     {
-        this.Code_Classe = Code_Classe;
+        this.code_Classe = code_Classe;
     }
 
     public String setType_Bien()
     {
-        this.Type_Bien = Type_Bien;
+        this.type_Bien = type_Bien;
     }
     
     public int setPrix_Maximum()
     {
-        this.Prix_Maximum = Prix_Maximum;
+        this.prix_Maximum = prix_Maximum;
     }
 }
