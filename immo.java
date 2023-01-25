e99c596a0fff8b77d7b3a6fec0adb396ff5f3ffc
Sylvain_C <scarlier@cutii.io>
2023-01-25 14:49:06 +0100

    ajout setters

--- immo.java
+++ immo.java
@@ -42,4 +42,28 @@ public class immo {
         return chargemensuel;
     }
 
+    public void setStatut(String statut) {
+        this.status = statut;
+    }
+
+    public void setLocalisation(String localisation) {
+        this.status = localisation;
+    }
+
+    public void setEtatdubien(String etatdubien) {
+        this.status = etatdubien;
+    }
+
+    public void setPrixachat(String prixachat) {
+        this.status = prixachat;
+    }
+
+    public void setLoyermensuel(String loyermensuel) {
+        this.status = loyermensuel;
+    }
+
+    public void setChargemensuel(String chargemensuel) {
+        this.status = chargemensuel;
+    }
+
 }
