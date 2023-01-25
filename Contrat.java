87d56bfd7b14f49e2a7eca322db22ae1573fd3c9
Domstang <arnaudlecamus@gmail.com>
2023-01-25 14:11:56 +0100

    feature: add class contrat

---
+++ Contrat.java
@@ -0,0 +1,19 @@
+public class Contrat {
+  private int numClient;
+  private String nom;
+  private String telephone;
+
+  public Contrat(int numClient, String nom, String telephone) {
+    this.numClient = numClient;
+    this.nom = nom;
+    this.telephone = telephone;
+  }
+
+  public int getNumClient() {
+    return numClient;
+  }
+
+  public void setNumClient(int numClient) {
+    this.numClient = numClient;
+  }
+}
