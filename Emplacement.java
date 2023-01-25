e8ca1c7636c9d3c73a2db5c476e5f27d2a73b1b5
= <49647013+yannick60@users.noreply.github.com>
2023-01-25 14:16:53 +0100

    ajout emplcement

---
+++ Emplacement.java
@@ -0,0 +1,13 @@
+public class Emplacement{
+
+private String adresse;
+private String nom;
+private String coordonnee;
+
+public Emplacement(){
+
+}
+
+public String getAdresse(){
+	return adresse;
+}
