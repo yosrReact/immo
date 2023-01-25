37b7f9895373ed702ba4f45dff1b8e91401ae33a
Baptiste <baptiste.defrance.dev@gmail.com>
2023-01-25 14:08:14 +0100

    add Appartement.java

---
+++ Appartement.java
@@ -0,0 +1,10 @@
+class Appartement : extends Bien-Immobilier {
+	private String description;
+	private int Loc_Etage;
+	private bool Ascenseur;
+	private bool cuisineEquipee;
+
+	void Appartement() {
+
+	}
+}
