ec513ac87c7e0e47591aba65dcae3f58986e8062
afournier-devops <alexandre.fournier@yahoo.fr>
2023-01-25 14:16:57 +0100

    add client.java

---
+++ client.java
@@ -0,0 +1,35 @@
+public class Client {
+    private int numClient;
+    private String nom;
+    private String telephone;
+    
+    public Client(int numClient, String nom, String telephone) {
+        this.numClient = numClient;
+        this.nom = nom;
+        this.telephone = telephone;
+    }
+
+    public int getNumClient() {
+        return numClient;
+    }
+
+    public void setNumClient(int numClient) {
+        this.numClient = numClient;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
