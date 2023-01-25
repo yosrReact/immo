7cc22d87600e990ed2b24ff6d327af4115739dab
= <49647013+yannick60@users.noreply.github.com>
2023-01-25 14:28:15 +0100

    correction suite code review

--- Emplacement.java
+++ Emplacement.java
@@ -1,13 +1,33 @@
 public class Emplacement{
 
-private String adresse;
-private String nom;
-private String coordonnee;
+	private String adresse;
+	private String nom;
+	private String coordonnee;
 
-public Emplacement(){
+	public Emplacement(){
 
-}
+	}
 
-public String getAdresse(){
-	return adresse;
-}
+	public String getAdresse(){
+		return adresse;
+	}
+	
+	public String getNom(){
+		return nom;
+	}
+
+	public String getCoordonnee(){
+		return coordonnee;
+	}
+
+	public void setAdresse(String s){
+                adresse=s;
+        }
+
+        public void setNom(String s){
+                nom=s;
+        }
+
+        public void setCoordonnee(String s){
+                coordonnee=s;
+        }
