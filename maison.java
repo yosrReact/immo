a5104bdadb5bedf21cd31bccacc1b0af16ad442f
saliha ouared <salihaouared@gmail.com>
2023-01-25 14:27:09 +0100

    add maison

---
+++ maison.java
@@ -0,0 +1,22 @@
+public class maison {
+	
+	int nombrechambre;
+	String destription;
+    String cuisineequipe;
+    String jardin;
+    
+	
+	public maison() {
+		nombrechambre=0 ;
+		destription="maison";
+		cuisineequipe = "non";
+		jardin = "non";
+		}
+		
+		
+	public String returninfo() {
+		
+		return "";
+	}
+
+}
